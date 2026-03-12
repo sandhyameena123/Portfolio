765c9b83c58b27563d3b13f39e55ff20844d4ce9
Sandhya Meena <sandhyameena33@gmail.com>
2026-03-12 11:21:03 +0530

    updates

---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+function Home(){
+    return(
+        <>
+      <section className="h-screen flex flex-col justify-center items-center bg-gray-100">
+      <h1 className="text-5xl font-bold">
+        Hi, I'm Sandhya
+      </h1>
+
+      <p className="mt-4 text-lg text-gray-600">
+        React Developer
+      </p>
+
+      <button className="mt-6 bg-blue-500 text-white px-6 py-2 rounded">
+        View Projects
+      </button>
+    </section>
+  
+        </>
+    );
+}
+
+export default Home;
